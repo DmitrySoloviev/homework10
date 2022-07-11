fd0e60d274baba810b1b6ab936410253746f5bcd
Дмитрий <DmitrySoloviev>
2022-07-11 07:37:06 +0300

    Changed utils.py

--- utils.py
+++ utils.py
@@ -1,37 +1,49 @@
 import json
 
 def load_candidates():
-
-    #Смотреть как из корня папки, а не диска C
+    """Загружает список кандидатов из файла"""
     with open('candidates.json', 'r', encoding='utf-8') as file:
         list_candidates = json.load(file)
         file.close()
         return list_candidates
 
 def get_all():
+    """Выводит список всех кандидатов"""
     list_candidates = load_candidates()
-    all_names = []
+    message = ""
     for candidate in list_candidates:
-        all_names.append(candidate["name"])
-    return all_names
+        message = message + candidate['name'] + "\n"
+        message = message + candidate['position'] + "\n"
+        message = message + candidate['skills'] + "\n" + "\n"
+    return message
+
+
 
 def get_by_pk(pk):
+    """Возвращает кандидатов по номеру"""
     list_candidates = load_candidates()
+    message = ""
     for candidate in list_candidates:
         if candidate["pk"] == pk:
-            return(candidate["name"])
+            picture_link = candidate['picture']
+            message = message + candidate['name'] + "\n"
+            message = message + candidate['position'] + "\n"
+            message = message + candidate['skills'] + "\n" + "\n"
         else:
             pass
-
+    return f"<img src='({picture_link})'>\n{message}"
 
 def get_by_skill(skill_name):
+    """Возвращает кандидатов по навыкам"""
     list_candidates = load_candidates()
-    candidates_with_skill = []
+    message = ""
     for candidate in list_candidates:
         list_skills = (candidate["skills"]).split(', ')
         if skill_name.lower() in list_skills or skill_name.title() in list_skills:
-            candidates_with_skill.append(candidate["name"])
+            message = message + candidate['name'] + "\n"
+            message = message + candidate['position'] + "\n"
+            message = message + candidate['skills'] + "\n" + "\n"
         else:
             pass
-    return candidates_with_skill
+    return message
 
